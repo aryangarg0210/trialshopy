1b8c0532927d65d76209c9782b1f0ec7dc4a1792
ARYAN-01-GARG <aryangarg30842@gmail.com>
2026-07-09 01:51:21 +0530

    feat(commerce): register order module in commerce

--- apps/server/src/commerce/commerce.module.ts
+++ apps/server/src/commerce/commerce.module.ts
@@ -3,9 +3,18 @@ import { AddressController } from "./address/address.controller";
 import { AddressService } from "./address/address.service";
 import { CartController } from "./cart/cart.controller";
 import { CartService } from "./cart/cart.service";
+import { OrderController } from "./order/order.controller";
+import { OrderService } from "./order/order.service";
+import { SubOrderController } from "./order/suborder.controller";
+import { SubOrderService } from "./order/suborder.service";
 
 @Module({
-	controllers: [AddressController, CartController],
-	providers: [AddressService, CartService],
+	controllers: [
+		AddressController,
+		CartController,
+		SubOrderController,
+		OrderController,
+	],
+	providers: [AddressService, CartService, OrderService, SubOrderService],
 })
 export class CommerceModule {}
